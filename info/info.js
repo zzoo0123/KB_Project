8401b78b01ede134cfe5d8994f39bca4305e5d8c
yeon-jae <68237481+yeon-jae@users.noreply.github.com>
2022-09-07 17:39:42 +0900

    2modified

--- info/info.js
+++
@@ -1,9 +0,0 @@
-const bars = document.querySelector(".bars");
-const menu = document.querySelector(".menu");
-const icon = document.querySelector(".icon");
-const user=document.querySelector(".user");
-
-bars.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  icon.classList.toggle("active");
-});
